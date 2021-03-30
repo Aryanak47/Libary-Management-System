6738976af6c482c609632b928ede592ba75f331f
Aryan Bimali <aryanbimali45@gmail.com>
2021-03-30 22:28:34 +0545

    initial commit 🚀

---
+++ app.js
@@ -0,0 +1,24 @@
+const book = {
+name,
+id,
+new,
+authors,
+description,
+number of stock,
+available
+
+
+}
+const user = {
+    name,
+    role,
+    email,
+    password
+}
+
+const userBook = {
+    bookid,
+    userid,
+    issueDate,
+    expiredDate
+}
